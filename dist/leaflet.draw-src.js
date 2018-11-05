ff730785db7fcccbf2485ffcf4dffe1238a7c617
Martijn van de Rijdt <martijn@enketo.org>
2018-11-05 11:21:22 -0700

    rebuilt with previous fix

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1,5 +1,5 @@
 /*
- Leaflet.draw 1.0.4, a plugin that adds drawing and editing tools to Leaflet powered maps.
+ Leaflet.draw 1.0.4+838a63b, a plugin that adds drawing and editing tools to Leaflet powered maps.
  (c) 2012-2017, Jacob Toye, Jon West, Smartrak, Leaflet
 
  https://github.com/Leaflet/Leaflet.draw
@@ -8,7 +8,7 @@
 (function (window, document, undefined) {/**
  * Leaflet.draw assumes that you have already included the Leaflet library.
  */
-L.drawVersion = "1.0.4";
+L.drawVersion = "1.0.4+838a63b";
 /**
  * @class L.Draw
  * @aka Draw
@@ -3154,6 +3154,7 @@ L.LatLngUtil = {
 		readableArea: function (area, isMetric, precision) {
 			var areaStr,
 				units,
+				type,
 				precision = L.Util.extend({}, defaultPrecision, precision);
 
 			if (isMetric) {
